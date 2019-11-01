bdfda24d3bdf070d9c9cad367ccdba7c5ff475cf
Oliver Young <oliver.young@digital.justice.gov.uk>
2019-11-01 10:35:42 +0000

    h_anonfuncs.go

--- playground/h_anonfuncs.go
+++ playground/h_anonfuncs.go
@@ -5,12 +5,16 @@ import (
 )
 
 func main() {
-
 	x := func() {
-		fmt.Println("This is an anonymous func")
+		fmt.Println("This is an anonymous func assigned to a variable")
 	}
-
-	//Run it
 	x()
-}
 
+	// Running an anonymous func directly
+	func() {
+		fmt.Println("Ths is an anon func to print decimal numbers 0 to 10 in binary:")
+		for z := 0; z <= 10; z++ {
+			fmt.Printf("%b\t", z)
+		}
+	}()
+}
