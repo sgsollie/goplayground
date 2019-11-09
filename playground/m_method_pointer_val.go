ddc672bb4e4380c3d8367471c0b45b087afc8ff3
Oliver Young <oliver.young@digital.justice.gov.uk>
2019-11-09 18:36:24 +0000

    updated comments

--- playground/m_method_pointer_val.go
+++ playground/m_method_pointer_val.go
@@ -13,8 +13,11 @@ type dog struct {
 
 // method to give dog a toy
 // this works by passing an unlimited amount of strings as an argument (identified as toy)
-// this is then added as a slice of string to the .toys field of the object
-// we have to use pointer to type dog as this is the correct way to update the value, not by passing it around
+// this is then added as a slice of string to the .toys field of the struct
+// we have to use pointer to type dog as this is the correct way to update the value, 
+// if we don't use a pointer, the value doesn't get updated as hoped, it just duplicates the value within the method 
+//& doesn't update the one referenced in main()
+
 func (d *dog) giveToy(toy ...string) {
 	//fmt.Println(toy)
 	(*d).toys = []string(toy)
